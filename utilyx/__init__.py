dd1f741955f672649ea580d6430c4459818b0ed8
alonefox1st-cmd <odamoliver00@gmail.com>
2026-03-21 14:35:21 +0000

    Update __init__.py for additional support to button functions

--- utilyx/__init__.py
+++ utilyx/__init__.py
@@ -1,3 +1,4 @@
 from .utilyx import *
+from .button_funtions import *
 
 __all__ = ["Print", "quit", "Exit", "loadconfig", "writeconfig", "writefile", "loadfile", "errorui", "delete_file", "clear_file", "native_error_ui", "load_file", "load_config", "write_file", "error_ui", "write_config", "nativeerrorui", "button_functions"]
