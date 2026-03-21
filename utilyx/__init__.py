b81547b51449de6b17334dc98b9b42d83c2b424d
alonefox1st-cmd <odamoliver00@gmail.com>
2026-03-21 12:09:03 +0000

    Update __init__.py
    Added alias

--- utilyx/__init__.py
+++ utilyx/__init__.py
@@ -1,3 +1,3 @@
 from .utilyx import *
 
-__all__ = ["Print", "quit", "Exit", "loadconfig", "writeconfig", "writefile", "loadfile", "errorui", "delete_file", "clear_file", "native_error_ui", "load_file", "load_config", "write_file", "error_ui", "write_config"]
+__all__ = ["Print", "quit", "Exit", "loadconfig", "writeconfig", "writefile", "loadfile", "errorui", "delete_file", "clear_file", "native_error_ui", "load_file", "load_config", "write_file", "error_ui", "write_config", "nativeerrorui"]
