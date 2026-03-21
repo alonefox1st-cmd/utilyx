7d184a0bedc7ff1c7882aa7188bb8ae06ec141aa
alonefox1st-cmd <odamoliver00@gmail.com>
2026-03-21 14:49:53 +0000

    Update __init__.py

--- utilyx/__init__.py
+++ utilyx/__init__.py
@@ -1,4 +1,4 @@
 from .utilyx import *
-from .button_functions import *
+from . import button_functions
 
 __all__ = ["Print", "quit", "Exit", "loadconfig", "writeconfig", "writefile", "loadfile", "errorui", "delete_file", "clear_file", "native_error_ui", "load_file", "load_config", "write_file", "error_ui", "write_config", "nativeerrorui", "button_functions"]
